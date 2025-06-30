4b1bd6989b4a85139ab9977678308f01fa54cf93
gibi1987 <gilbert.senoirb@gmail.com>
2025-06-30 04:00:25 +0000

    PalindromeCheck.java

---
+++ Task-Activity/PalindromeCheck.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Reverse the string using StringBuilder
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        // Compare original with reversed
+        if (input.equals(reversed)) {
+            System.out.println("The input string is a palindrome.");
+        } else {
+            System.out.println("The input string is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
